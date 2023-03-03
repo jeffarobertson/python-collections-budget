ab7912ea7d227a57d63fc1149060c728dbb9e419
jeffrobertson <jrobertson@motivhealth.com>
2023-03-03 09:05:41 -0700

    update FrequentExpenses.py

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -13,4 +13,6 @@ for expense in expenses.list:
 
 spending_counter = collections.Counter(spending_categories)
 
-print(spending_counter)
+top5 = spending_counter.most_common(5)
+
+categories, count = zip(*top5)
