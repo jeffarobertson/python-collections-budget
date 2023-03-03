1c2fe53dfd3e06f61676bdba68c46084e7df7f15
jeffrobertson <jrobertson@motivhealth.com>
2023-03-03 10:12:58 -0700

    update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -49,7 +49,7 @@ def main():
     fig, ax = plt.subplots()
 
     labels = ['Expenses', 'Overages', 'Budget']
-    values = [myBudgetList.expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
     ax.bar(labels, values, color=['green', 'red', 'blue'])
     ax.set_title('Your total expenses vs. total budget')
     plt.show()
