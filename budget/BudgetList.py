83d9c595e29bf1150827ec28a65ec0d3798fded9
jeffrobertson <jrobertson@motivhealth.com>
2023-03-03 09:30:18 -0700

    update BudgetList.py

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -25,3 +25,7 @@ def main():
     myBudgetList = BudgetList(1200)
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
+    
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+        print('The count of all expenses: ' + str(len(myBudgetList))
