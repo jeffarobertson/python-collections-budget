7ecffab6de7fc8286fd05c16749a484fe6b1dd5c
jeffrobertson <jrobertson@motivhealth.com>
2023-03-03 08:42:11 -0700

    update FrequentExpenses.py

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -5,11 +5,11 @@ from . import Expense
 
 expenses = Expense.Expenses().read_expenses('data/spending_data.csv')
 
-spending_categories = []
+# spending_categories = []
 
-for expense in expenses.list:
-    spending_categories.append(expense.category)
+# for expense in expenses.list:
+#     spending_categories.append(expense.category)
 
-spending_counter = collections.Counter(spending_categories)
+# spending_counter = collections.Counter(spending_categories)
 
-print(spending_counter)
+# print(spending_counter)
