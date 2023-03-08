7ddaeee1fbf9f7a3585ed847b6e2d589e48599bd
jeffrobertson <jrobertson@motivhealth.com>
2023-03-08 11:52:31 -0700

    update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -7,20 +7,38 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
     divided_set_comp = expenses.categorize_set_comprehension()
+
     if not divided_set_comp == divided_for_loop:
         print("Sets are NOT equal by == test")
+
     for a, b in zip(divided_for_loop, divided_set_comp):
         if not (a.issubset(b) and b.issubset(a)):
             print("Sets are NOT equal by subset test")
-    timeit.timeit(
-        stmt="expenses.categorize_for_loop()",
-        setup='''
-            from . import Expense
-            expenses = Expense.Expenses()
-            expenses.read_expenses('data/spending_data.csv')
-        ''',
-        number=100000,
-        globals=globals()
+
+    print(
+        timeit.timeit(
+            stmt="expenses.categorize_for_loop()",
+            setup='''
+                from . import Expense
+                expenses = Expense.Expenses()
+                expenses.read_expenses('data/spending_data.csv')
+            ''',
+            number=100000,
+            globals=globals()
+        )
+    )
+
+    print(
+        timeit.timeit(
+            stmt="expenses.categorize_set_comp()",
+            setup='''
+                from . import Expense
+                expenses = Expense.Expenses()
+                expenses.read_expenses('data/spending_data.csv')
+            ''',
+            number=100000,
+            globals=globals()
+        )
     )
 
 
